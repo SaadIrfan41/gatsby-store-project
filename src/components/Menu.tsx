ef5904ed1895dee24bf846a08678ad27d11c74d8
saix511 <saadirfan41@gmail.com>
2020-12-14 00:43:15 +0500

    Menu improved

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { Fragment, useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import { Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap"
+import { Button, Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap"
 import Img from "gatsby-image"
 import { FixedObject } from "../interface"
 
@@ -15,7 +15,24 @@ interface nodeprops {
   image: {
     fixed: FixedObject | FixedObject[]
   }
+  map:any;
+  
+  
 }
+
+
+// const getCategories = (items:nodeprops) =>{
+//   let tempitems= items.map((items:nodeprops)=>{
+//       return items.category
+//   })
+//   // Using set Method to take A single category from Multiple same category items
+//   let tempcategories= new Set(tempitems);
+//   let categories =  Array.from(tempcategories)
+
+//   categories = ["all",...categories]
+//     return categories;
+// }
+
 
 const Menu = () => {
   const data = useStaticQuery(
@@ -40,7 +57,32 @@ const Menu = () => {
       }
     `
   )
-  console.log(data.Menu.nodes)
+  
+//   const categories = getCategories(data.Menu.nodes)
+// console.log(categories)
+
+  const [menuitems, setmenuitems] = useState(data.Menu.nodes);
+
+
+ const handelcategory=(category:string)=>{
+   let tempitem=data.Menu.nodes;
+  if(category==="All"){
+    return setmenuitems(data.Menu.nodes);
+  }
+      else{
+            let item = tempitem.filter((node:nodeprops)=>
+              node.category=== category
+            )
+            
+            return setmenuitems(item);
+      }
+
+
+ }
+
+    
+
+
 
   return (
     <section className="text-center py-5 menu">
@@ -50,9 +92,31 @@ const Menu = () => {
             Menu Items are Displayed Below
           </h1>
           <Row>
-            {data.Menu.nodes.map((node: nodeprops) => {
+            <Col>
+                  <Button  className="m-3 mb-5" onClick={() =>{handelcategory("All")}}>All</Button>
+                  {
+                    data.Menu.nodes.map((node: nodeprops)=>{
+                          return(
+                            
+                            
+                              <Button className="m-3 mb-5" key={node.id} onClick={() =>{handelcategory(node.category)}}>
+
+                                {node.category}
+
+                              </Button>
+                              
+
+                          )
+                            
+                          
+                    })
+                  }
+            </Col>
+          </Row>
+          <Row>
+            {menuitems.map((node: nodeprops) => {
               return (
-                <>
+                <Fragment key={node.id}>
                   <Col md={3}>
                     <Img fixed={node.image.fixed} />
                   </Col>
@@ -72,7 +136,7 @@ const Menu = () => {
                       
                     </ListGroup>
                   </Col>
-                </>
+                </Fragment>
               )
             })}
           </Row>
